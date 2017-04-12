33fa52c2b077bf0fd26b78edb863db052fe8b9d3
Jeff Yamada <jeff@impossible-bureau.com>
2017-04-12 02:55:36 -0600

    adjusted columns for mobile

--- src/scripts/components/Circles.js
+++ src/scripts/components/Circles.js
@@ -22,19 +22,29 @@ export default class Circles extends Component {
     this.maxDistance = 0;
     this.introProgressBlack = 0;
     this.introProgressWhite = 0;
+    this.isDirty = true;
     this.prepPixi();
     this.onResize();
     this.update();
 
     window.addEventListener('mousemove', this.onMouseMove);
     window.addEventListener('resize', this.onResize);
+    window.addEventListener("touchstart", this.onTouchStart, false);
+    window.addEventListener("touchmove", this.onTouchMove, false);
+    // window.addEventListener('deviceorientation', this.onOrientationChange, true);
 
     TweenMax.to(this, 2, {
       introProgressBlack: 1,
+      onUpdate: () => {
+        this.isDirty = true;
+      },
     });
     TweenMax.to(this, 2, {
       introProgressWhite: 1,
       delay: 1,
+      onUpdate: () => {
+        this.isDirty = true;
+      },
     });
   }
 
@@ -42,18 +52,59 @@ export default class Circles extends Component {
     TweenMax.to(this.mousePos, 1, {
       x: event.clientX,
       y: event.clientY,
+      onUpdate: () => {
+        this.isDirty = true;
+      },
     });
   };
+
+  onTouchStart = (event) => {
+    const touch = event.touches[0];
+    TweenMax.to(this.mousePos, 1, {
+      x: touch.clientX,
+      y: touch.clientY,
+      onUpdate: () => {
+        this.isDirty = true;
+      },
+    });
+  };
+
+  onTouchMove = (event) => {
+    const touch = event.touches[0];
+    TweenMax.to(this.mousePos, 1, {
+      x: touch.clientX,
+      y: touch.clientY,
+      onUpdate: () => {
+        this.isDirty = true;
+      },
+    });
+  }
 
   onResize = () => {
     this.winWidth = window.innerWidth;
     this.winHeight = window.innerHeight;
+    Circles.PADDING = Math.min(this.winHeight, this.winWidth) * 0.1;
+    Circles.COLS = Math.min(20, Math.round(this.winHeight / 30));
+    if (this.winHeight > this.winWidth) {
+      Circles.COLS = Math.min(8, Circles.COLS);
+    }
     this.pixiRenderer.resize(this.winWidth, this.winHeight);
     this.maxDistance = Point.distance({ x: this.winWidth, y: this.winHeight });
     this.circleGridSize = (window.innerWidth - (Circles.PADDING * 2)) / Circles.COLS;
     this.createCircles();
     this.drawCircles();
+    this.isDirty = true;
   };
+
+  onOrientationChange = (event) => {
+    TweenMax.to(this.mousePos, 1, {
+      x: Math.abs(event.alpha),
+      y: Math.abs(event.clientY),
+      onUpdate: () => {
+        this.isDirty = true;
+      },
+    });
+  }
 
   drawCircles = () => {
     const circleSize = this.circleGridSize * 0.8;
@@ -79,12 +130,19 @@ export default class Circles extends Component {
         TweenMax.to(circle, 1, {
           introFrontProgress: 1,
           delay: 1 * centerRatio,
+          onUpdate: () => {
+            this.isDirty = true;
+          },
         });
         TweenMax.to(circle, 1, {
           introBackProgress: 1,
           delay: (1 * centerRatio) + 0.5,
+          onUpdate: () => {
+            this.isDirty = true;
+          },
         });
         circle.started = true;
+        this.isDirty = true;
       }
 
       circle.background.width = circleSize * circle.introFrontProgress;
@@ -147,8 +205,11 @@ export default class Circles extends Component {
   };
 
   update = () => {
-    this.drawCircles();
-    this.pixiRenderer.render(this.stage);
+    if (this.isDirty) {
+      this.drawCircles();
+      this.pixiRenderer.render(this.stage);
+      this.isDirty = false;
+    }
     this.raf = window.requestAnimationFrame(this.update);
   }
 
